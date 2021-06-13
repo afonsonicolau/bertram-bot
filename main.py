16d41a7d5d3f48461e2247fe5886d6b3678d7b9c
nicolau <afonso9980@gmail.com>
2021-06-13 17:44:28 +0100

    Separated sql functions and method to another files

--- main.py
+++ main.py
@@ -3,34 +3,28 @@ import re
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import mysql.connector
-from mysql.connector import Error
+import sql
 
 # Load JSON Secrets
 secrets = open('secrets.json')
 secretdata = json.load(secrets)
 
-# SQL Connection
-try:
-    connection = mysql.connector.connect(host='localhost', database='bertramtest', user='bertram', password=secretdata['bertramredentials'])
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        connection.close()
-
 # Spreadsheet scope and sheet
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-credentials = ServiceAccountCredentials.from_json_keyfile_dict(secretdata['googlecredentials'], scope)
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(
+    secretdata['googlecredentials'], scope)
 clientsheet = gspread.authorize(credentials)
 sheet = clientsheet.open('Modelos - TNLRP').sheet1
 
 # Discord bot client
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-  print('Hello World, I am {0.user}'.format(client))
+    print('Hello World, I am {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -38,16 +32,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('<@!852648286602919964>'):
+    if message.content.startswith('<@!852648286602919964>') or message.content.startswith('<@852648286602919964>'):
+
         usermessage = message.content
         messagesplitted = usermessage.split()
+        is_manager = message.author.id in secretdata['projects']['tnlrp']['managers']
 
-        botcommand = ''
-        botcommand = messagesplitted[1] if 0 <= 1 < len(messagesplitted) else 'invalid'
+        botcommand = messagesplitted[1] if 1 < len(
+            messagesplitted) else 'invalid'
 
-        if botcommand == 'darbote' and len(messagesplitted) >= 4:
+        if botcommand == 'darbote' and len(messagesplitted) >= 4 and is_manager:
             steamid = messagesplitted[2]
-            plate = messagesplitted[4] if 0 <= 4 < len(messagesplitted) else None
+            plate = messagesplitted[4] if 4 < len(messagesplitted) else None
             carmodel = None
             models = sheet.col_values(4)
             for model in models:
@@ -64,35 +60,52 @@ async def on_message(message):
         elif botcommand == 'invalid':
             await embededmessages(message, "Inválida", "Erro", "Acho que se escreveres algo válido funciona.")
 
+
 async def givecar(steamid, carmodel, carname, plate, vehicleprops, message):
-    # Verify in database first if steamid is valid.
-    connection.connect()
-    if connection.is_connected():
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE identifier = %(identifier)s", { 'identifier': steamid })
-        record = cursor.fetchone()
-        if record == None:
+    if sql.open_connection():
+        cursor = sql.connect_cursor()
+        cursor.execute(
+            "SELECT * FROM users WHERE identifier = %(identifier)s", {'identifier': steamid})
+        player_data = cursor.fetchone()
+
+        if player_data == None:
             await embededmessages(message, "Dar um bote", "Erro", "Não existe sequer uma pessoa com o 'steamid' inserido porra.")
         else:
             if plate is None:
-                plate = "69AIB696"
+                plate = "A3123"
 
-            playername = record[1]
-            await embededmessages(message, "Dar um bote", "Sucesso", "O tal '" + carname + "' de matrícula '" + plate + "' foi dado ao " + playername + ", ok?")
+                cursor.execute(
+                    "SELECT plate FROM owned_vehicles WHERE plate = %(plate)s", {'plate': plate})
+                plate_exists = cursor.fetchone()
 
-        connection.close()
+                if plate_exists is None:
+
+                    sql.insert_data(
+                        "INSERT INTO owned_vehicles (identifier, plate, vehicleprops) VALUES(%(identifier)s, %(plate)s, %(vehicleprops)s);",
+                        {'identifier': steamid, 'plate': plate, 'vehicleprops': vehicleprops}
+                    )
+
+                    playername = player_data[1]
+                    await embededmessages(message, "Dar um bote", "Sucesso", "O tal '" + carname + "' de matrícula '" + plate + "' foi dado ao " + playername + ", ok?")
+                else:
+                    await embededmessages(message, "Dar um bote", "Erro", "A matrícula '" + plate + "' já existe, logo não dá, né.")
+
+        sql.close_connection()
+
 
 async def embededmessages(message, action, state, description):
     # Prepare discord embeded message
-    embed=discord.Embed(title="Bertram - O Mordomo")
+    embed = discord.Embed(title="Bertram - O Mordomo")
 
     if state == "Sucesso":
-        embed.color=discord.Color.green()
+        embed.color = discord.Color.green()
     elif state == "Erro":
-        embed.color=discord.Color.red()
+        embed.color = discord.Color.red()
 
-    embed.set_thumbnail(url="https://static.wikia.nocookie.net/disneyjessieseries/images/3/3e/J110.jpg/revision/latest/scale-to-width-down/286?cb=20120324031248")
-    embed.add_field(name="Pedido feito por", value=message.author.name + "#" + message.author.discriminator, inline=False)
+    embed.set_thumbnail(
+        url="https://static.wikia.nocookie.net/disneyjessieseries/images/3/3e/J110.jpg/revision/latest/scale-to-width-down/286?cb=20120324031248")
+    embed.add_field(name="Pedido feito por", value=message.author.name +
+                    "#" + message.author.discriminator, inline=False)
     embed.add_field(name="Ação", value=action, inline=True)
     embed.add_field(name="Estado", value=state, inline=True)
     embed.add_field(name="Mensagem", value=description, inline=False)
---
+++ sql.py
@@ -0,0 +1,44 @@
+import json
+import mysql.connector
+
+# Load JSON Secrets
+secrets = open('secrets.json')
+secretdata = json.load(secrets)
+
+connection = mysql.connector.MySQLConnection(host=secretdata['bertramcredentials']['host'],
+                                     database=secretdata['bertramcredentials']['database'],
+                                     user=secretdata['bertramcredentials']['user'],
+                                     password=secretdata['bertramcredentials']['password'])
+cursor = connection.cursor()
+
+def open_connection():
+    try:
+        connection.connect()
+        if connection.is_connected():
+            return True
+    except mysql.connector.Error as e:
+        return False
+
+
+def connect_cursor():
+    if connection.is_connected():
+        cursor = connection.cursor()
+        return cursor
+
+# Function that inserts data into said table
+def insert_data(insert_query, data):
+    try:
+        # Executing the SQL command
+        cursor.execute(insert_query, data)
+
+        # Commit your changes in the database
+        connection.commit()
+
+    except:
+        # Roll back in case of error
+        connection.rollback()
+
+def close_connection():
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
