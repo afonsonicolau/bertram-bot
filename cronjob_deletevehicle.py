ebc3a1ce2eafce7798d10d539c1f5491a89756c0
nicolau <afonso9980@gmail.com>
2021-06-16 15:12:30 +0100

    Finished cronjob to delete vehicles, modified README

--- cronjob_deletevehicle.py
+++ cronjob_deletevehicle.py
@@ -1,31 +1,27 @@
+#!/home/nicolau/git/bertram-bot/bertram-bot/bin/python3
+
 # This cronjob checks the "is_deleted" column for timestamps that have exceed 30 days to delete the row
 import sql
 from datetime import datetime
 
+
 def check_timestamp():
     if sql.open_connection():
         date_now = datetime.today()
-        date_now = date_now.strftime('%Y/%m/%d')
+        date_now = date_now.strftime('%Y/%m/%d %H:%M')
 
         cursor = sql.connect_cursor()
-        cursor.execute("SELECT plate FROM owned_vehicles WHERE is_deleted IS NOT NULL AND DATEDIFF(DATE(NOW()), DATE(is_deleted)) >= 0")
+        cursor.execute("SELECT plate FROM owned_vehicles WHERE is_deleted IS NOT NULL AND DATEDIFF(DATE(NOW()), DATE(is_deleted)) >= 30")
         vehicles = cursor.fetchall()
 
-        log_file = open("cronjob_logs.txt", "a")
+        log_file = open("/tmp/cronjob_logs.txt", "a")
+        vehicle_plates = [plate[0] for plate in vehicles]
 
         if len(vehicles) > 0:
-            print(vehicles)
             log_file.writelines("\n\n***Cronjob - Delete expired vehicles*** - " + date_now)
+            log_file.writelines("\nThe following vehicles were deleted from the database: " + ", ".join(vehicle_plates))
 
-            i = 1
-            while i <= len(vehicles):
-                for plate in vehicles[i - 1]:
-                    print(plate)
-                    log_file.writelines("\nThe vehicle with the plate '" + plate + "' was deleted from the database.")
-                    query = "DELETE FROM owned_vehicles WHERE is_deleted IS NOT NULL AND DATEDIFF(DATE(NOW()), DATE(is_deleted)) > 30"
-                    break
-
-                i += 1
+            sql.run_query("CALL delete_vehicle()", '')
         else:
             log_file.writelines("\n\n***Cronjob - Delete expired vehicles*** - " + date_now + "\n No cars were deleted on this day.")
 
@@ -33,18 +29,5 @@ def check_timestamp():
 
     sql.close_connection()
 
-if __name__ == "__main__":
-    check_timestamp()
 
-""" from crontab import CronTab
-from datetime import datetime
-
-cron_user = CronTab(user='nicolau')
-
-job = cron_user.new(command='python home/nicolau/git/bertram-bot/cronjob_deletevehicle.py')
-job.minute.every(1)
-
-for job in cron_user:
-    myFile = open('crontab_logs.txt', 'a')
-    myFile.write('\nAccessed on ' + str(datetime.now()))
-    print(job) """
+check_timestamp()
