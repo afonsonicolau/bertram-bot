8ba8905e0504f6284108b553db371e9005147b86
nicolau <afonso9980@gmail.com>
2021-08-03 12:57:05 +0100

    Add help and changed how players are looked up

--- player_characters.py
+++ player_characters.py
@@ -5,14 +5,14 @@ import messages
 async def get_character(steamid, message):
     if sql.open_connection():
         cursor = sql.connect_cursor()
-        cursor.execute("SELECT * FROM users WHERE identifier LIKE %(steamid)s", {'steamid': '%' + steamid})
+        cursor.execute("SELECT identifier, firstname, lastname FROM users WHERE identifier LIKE %(steamid)s", {'steamid': '%' + steamid})
         characters = cursor.fetchall()
 
         if len(characters) > 0:
             formatted_characters = ""
 
             for character in characters:
-                formatted_characters += "\nIdentificador - " + character[0] + " | Nome - " + character[1]
+                formatted_characters += "\nIdentificador - " + character[0] + " | Nome - " + character[1] + " " + character[2]
 
             await messages.embeded_messages(message, "Personagens de um jogador", "Sucesso", "As seguintes personagens foram encontradas: \n" + formatted_characters)
         else:
