2621096f0932b6bf9cca6c5b63da027ed0667015
afonso jose nicolau <a.nicolau@kigroup.de>
2022-09-09 16:49:52 +0100

    refactor: change sql and ssh services location

---
+++ services/sql.py
@@ -0,0 +1,45 @@
+import jsoner.json_files as json_files
+import mysql.connector
+
+connection = mysql.connector.MySQLConnection(host=json_files.get_field('projects.tnlrp.dbcredentials.host'),
+                                             database=json_files.get_field('projects.tnlrp.dbcredentials.database'),
+                                             user=json_files.get_field('projects.tnlrp.dbcredentials.user'),
+                                             password=json_files.get_field('projects.tnlrp.dbcredentials.password'))
+cursor = connection.cursor(dictionary=True)
+
+
+def open_connection():
+    try:
+        connection.connect()
+        if connection.is_connected():
+            return True
+    except mysql.connector.Error as e:
+        return False
+
+
+def connect_cursor():
+    if connection.is_connected():
+        cursor = connection.cursor(buffered=True)
+        return cursor
+
+# Function that inserts data into said table
+
+
+def run_query(query, data):
+    try:
+        # Executing the SQL command
+        cursor.execute(query, data)
+
+        # Commit your changes in the database
+        connection.commit()
+
+    except mysql.connector.Error as e:
+        print(e)
+        # Roll back in case of error
+        connection.rollback()
+
+
+def close_connection():
+    if connection.is_connected():
+        connection.close()
+        cursor.close()
---
+++ services/ssh_connection.py
@@ -0,0 +1,30 @@
+import jsoner.json_files as json_files
+# SSH library
+import paramiko
+
+ssh_client = paramiko.SSHClient()
+
+
+def open_connection():
+    # SSH private key
+    ssh_private_key = paramiko.RSAKey.from_private_key_file('/home/nicolau/.ssh/id_rsa')
+
+    ssh_client.load_host_keys('/home/nicolau/.ssh/known_hosts')
+    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+
+    ssh_client.connect(hostname=json_files.get_field('ssh.ip'),
+                       port=22,
+                       username=json_files.get_field('ssh.username'),
+                       pkey=ssh_private_key)
+
+    stdin, stdout, stderr = ssh_client.exec_command('hostname')
+    print(f'STDOUT: {stdout.read().decode("utf8")}')
+
+    stdin.close()
+    stdout.close()
+    stderr.close()
+
+
+def close_connection():
+    ssh_client.close()
